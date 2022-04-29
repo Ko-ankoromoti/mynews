8701bc1cfcaa98348973f4a877eb3f285cd2f389
ko-ankoromoti <naha.ameku.1965zai@gmeil.com>
2022-04-29 06:39:10 +0000

    Laravel 10

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -14,3 +14,4 @@ class NewsController extends Controller
   }
   
 }
+
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,46 +12,18 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-}
 
-
-namespace App\Http\Controllers\Admin;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class ProfileController extends Controller
-{
     //
     public function create()
     {
         return redirect('admin/profile/create');
     }
-}
 
-
-namespace App\Http\Controllers\Admin;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class ProfileController extends Controller
-{
     //
     public function edit()
     {
         return view('admin.profile.edit');
     }
-}
-
-
-namespace App\Http\Controllers\Admin;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
-class ProfileController extends Controller
-{
     //
     public function updete()
     {
